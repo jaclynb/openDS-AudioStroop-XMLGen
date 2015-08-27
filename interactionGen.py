9b651293963f459a3fe2797eca99d0b6e5ab0a10
jaclynb <jaclynb@mtu.edu>
2015-08-27 13:55:41 -0400

    Refactored to be more modular

--- interactionGen.py
+++ interactionGen.py
@@ -1,167 +1,189 @@
 #!/usr/bin/env python
 
-def makeActivity(triggerId, ref, contents):
-	s = '\t\t<activity id="' + triggerId + '" ref="' + ref + '">\n'
-	s += contents
-	s += '\t\t</activity>\n'
-	return s
+prologue = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n' + \
+'<interaction xmlns="http://opends.eu/drivingtask/interaction" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://opends.eu/drivingtask/interaction ../../Schema/interaction.xsd">\n'
 
-def makeActivtyRefOnly(ref):
-	return '<activity ref="' + ref + '"/>\n'
+activitiesOpenTag = '\t<activities>\n'
 
-def makeActionNoParam(delay, triggerId, repeat):
-	return '\t\t\t<action delay="' + delay + '" id="' + triggerId + '" repeat="' + repeat + '"/>\n'
+startActivities = '\t\t<activity id="Start_startRM" ref="">\n\
+\t\t\t<action delay="0.0" id="startReactionMeasurement" repeat="1"/>\n\
+\t\t</activity>\n\
+\t\t<activity id="Start_startRecording" ref="">\n\
+\t\t\t<action delay="0.0" id="startRecording" repeat="4">\n\
+\t\t\t\t<parameter name="track" value="1"/>\n\
+\t\t\t</action>\n\
+\t\t</activity>\n\
+\t\t<activity id="End_stopRecording" ref="">\n\
+\t\t</activity>\n\
+\t\t<activity id="End_pauseSimulation" ref="">\n\
+\t\t\t<action delay="0.0" id="pauseSimulation" repeat="4">\n\
+\t\t\t\t<parameter name="duration" value="0"/>\n\
+\t\t\t</action>\n\
+\t\t</activity>\n'
 
-def makeAction(delay, triggerId, repeat, params):
-	s = '\t\t\t<action delay="' + delay + '" id="' + triggerId + '" repeat="' + repeat + '">\n'
-	s += params
-	s += '\t\t\t</action>\n'
-	return s
+activitiesCloseTag = '\t</activities>\n'
 
-def makeParameter(name, value):
-	s = '\t\t\t\t<parameter name="' + name + '" value="' + value + '"/>\n'
-	return s
+triggersOpenTag = '\t<triggers>\n'
 
-def makeTrigger(triggerId, priority, contents):
-	s = '\t\t\t<trigger id="' + triggerId + '" priority="' + priority + '">\n'
-	s += contents
-	s += '\t\t\t</trigger>\n'
-	return s
+startTrig = '\t\t<trigger id="BLC_Test_Start_Trigger" priority="1">\n \
+\t\t\t<activities>\n\
+\t\t\t\t<activity ref="Start_startRM"/>\n\
+\t\t\t\t<activity ref="Start_startRecording"/>\n\
+\t\t\t</activities>\n\
+\t\t\t<condition>collideWith:BLC_Test_Start_Trigger</condition>\n\
+\t\t</trigger>\n'
 
-def makeTriggerContents(activities, conditions):
-	return activities + conditions
+endTrig = '\t\t<trigger id="BLC_Test_End_Trigger" priority="1">\n\
+\t\t\t<activities>\n\
+\t\t\t\t<activity ref="End_stopRecording"/>\n\
+\t\t\t\t<activity ref="End_pauseSimulation"/>\n\
+\t\t\t</activities>\n\
+\t\t\t<condition>collideWith:BLC_Test_End_Trigger</condition>\n\
+\t\t</trigger>\n'
 
-def makeCondition(contents):
-	return '<condition>' + contents + '</condition>\n'
+triggerCloseTag = '\t</triggers>\n'
 
-version = raw_input("Version number? ")
-
-print ("Remember to rename the file to scene.xml or change the file name in AudioStroop.xml before using.")
-
-if version != "":
-	filename = "interaction-" + version + ".xml"
-else:
-	filename = "interaction.xml"
-
-code = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n'
-code += '<interaction xmlns="http://opends.eu/drivingtask/interaction" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://opends.eu/drivingtask/interaction ../../Schema/interaction.xsd">\n'
-code += '\t<activities>\n'
-code += '\t\t<activity id="Start_startRM" ref="">\n'
-code += '\t\t\t<action delay="0.0" id="startReactionMeasurement" repeat="1"/>\n'
-code += '\t\t</activity>\n'
-code += '\t\t<activity id="Start_startRecording" ref="">\n'
-code += '\t\t\t<action delay="0.0" id="startRecording" repeat="4">\n'
-code += '\t\t\t\t<parameter name="track" value="1"/>\n'
-code += '\t\t\t</action>\n'
-code += '\t\t</activity>\n'
-code += '\t\t<activity id="End_stopRecording" ref="">\n'
-code += '\t\t\t<action delay="0.0" id="stopRecording" repeat="4"/>\n'
-code += '\t\t</activity>\n'
-code += '\t\t<activity id="End_pauseSimulation" ref="">\n'
-code += '\t\t\t<action delay="0.0" id="pauseSimulation" repeat="4">\n'
-code += '\t\t\t\t<parameter name="duration" value="0"/>\n'
-code += '\t\t\t</action>\n'
-code += '\t\t</activity>\n'
+epilogue = '</interaction>\n'
 
 targetLaneList = ["2", "1", "0", "2", "0", "2", "1", "2", "0", "2", "0", "1", "2", "1", "0", "1", "0", "1"]
+
 startLaneList =  ["1", "2", "1", "0", "2", "0", "2", "1", "2", "0", "2", "0", "1", "2", "1", "0", "1", "0"]
+
 soundIDList = ["left1", "right1", "right1", "left2", "right2", "left2", "right1", "left1", "right2", \
-				"left2", "right2", "left1", "left1", "right1", "right1", "left1", "right1", "left1"]
+                "left2", "right2", "left1", "left1", "right1", "right1", "left1", "right1", "left1"]
 
-for i in range(18):
-	sign = "BLC_Test_Block0_Trial" + str(i) + "_ContainerElement0"
-	params = makeParameter("visible", "true")
-	params += makeParameter("id", sign)
-	contents = makeAction("0.0", "manipulateObject", "1", params)
-	
-	sign = "BLC_Test_Block0_Trial" + str(i) + "_ContainerElement1"
-	params = makeParameter("visible", "true")
-	params += makeParameter("id", sign)
-	contents += makeAction("0.0", "manipulateObject", "1", params)
-	
-	sign = "BLC_Test_Block0_Trial" + str(i) + "_ContainerElement2"
-	params = makeParameter("visible", "true")
-	params += makeParameter("id", sign)
-	contents += makeAction("0.0", "manipulateObject", "1", params)
-	
-	activityId = "BLC_Test_Block0_Trial" + str(i) + "_Trigger0_makeVisible_BLC_Test_Block0_Trial" + str(i) + "_ContainerElements"
-	code += makeActivity(activityId, "", contents)
+def makeActivity(triggerId, ref, contents):
+    activity = '\t\t<activity id="' + triggerId + '" ref="' + ref + '">\n'
+    activity += contents
+    activity += '\t\t</activity>\n'
+    return activity
 
-	params = makeParameter("minSteeringAngle", "0")
-	params += makeParameter("targetLane", targetLaneList[i])
-	params += makeParameter("startLane", startLaneList[i])
-	params += makeParameter("taskCompletionAfterDistance", "117")
-	params += makeParameter("taskCompletionAfterTime", "7000")
-	params += makeParameter("timerID", "timer1")
-	params += makeParameter("allowBreak", "true")
-	params += makeParameter("successSound", "good")
-	params += makeParameter("congruenceClass", "LaneChange")
-	params += makeParameter("holdLaneFor", "2000")
-	params += makeParameter("failSound", "fail")
-	contents = makeAction("0.0", "setupLaneChangeReactionTimer", "1", params)
-	
-	activityId = "BLC_Test_Block0_Trial" + str(i) + "_Trigger0_setupBrakeLaneChangeReactionTimer"
-	code += makeActivity(activityId, "", contents)
+def makeActivtyRefOnly(ref):
+    return '<activity ref="' + ref + '"/>\n'
 
-	"""
-	params = makeParameter("soundID", soundIDList[i]);
-	contents = makeAction("0.0", "playSound", "1", params);
-	activityId = "BLC_Test_Block0_Trial" + str(i) + "_Trigger0_Sound_Stimulus"
-	code += makeActivity(activityId, "", contents)
-	"""
+def makeActionNoParam(delay, triggerId, repeat):
+    return '\t\t\t<action delay="' + delay + '" id="' + triggerId + '" repeat="' + repeat + '"/>\n'
 
-code += '\t</activities>\n'
-code += '\t<triggers>\n'
-code += '\t\t<trigger id="BLC_Test_Start_Trigger" priority="1">\n'
-code += '\t\t\t<activities>\n'
-code += '\t\t\t\t<activity ref="Start_startRM"/>\n'
-code += '\t\t\t\t<activity ref="Start_startRecording"/>\n'
-code += '\t\t\t</activities>\n'
-code += '\t\t\t<condition>collideWith:BLC_Test_Start_Trigger</condition>\n'
-code += '\t\t</trigger>\n'
-code += '\t\t<trigger id="BLC_Test_End_Trigger" priority="1">\n'
-code += '\t\t\t<activities>\n'
-code += '\t\t\t\t<activity ref="End_stopRecording"/>\n'
-code += '\t\t\t\t<activity ref="End_pauseSimulation"/>\n'
-code += '\t\t\t</activities>\n'
-code += '\t\t\t<condition>collideWith:BLC_Test_End_Trigger</condition>\n'
-code += '\t\t</trigger>\n'
+def makeAction(delay, triggerId, repeat, params):
+    action = '\t\t\t<action delay="' + delay + '" id="' + triggerId + '" repeat="' + repeat + '">\n'
+    action += params
+    action += '\t\t\t</action>\n'
+    return action
 
-for i in range(18):
-	refElements = "BLC_Test_Block0_Trial" + str(i) + "_Trigger0_makeVisible_BLC_Test_Block0_Trial" + str(i) + "_ContainerElements"
-	refReactionTimer = "BLC_Test_Block0_Trial" + str(i) + "_Trigger0_setupBrakeLaneChangeReactionTimer"
-	activities = "\t\t\t\t<activities>\n"
-	activities += "\t\t\t\t\t" + makeActivtyRefOnly(refElements)
-	activities += "\t\t\t\t\t" + makeActivtyRefOnly(refReactionTimer)
-	activities += "\t\t\t\t</activities>\n"
+def makeParameter(name, value):
+    param = '\t\t\t\t<parameter name="' + name + '" value="' + value + '"/>\n'
+    return param
 
-	conditionContents = "collideWith:BLC_Test_Block0_Trial" + str(i) +"_Trigger0"
-	conditions = "\t\t\t\t" + makeCondition(conditionContents)
+def makeTrigger(triggerId, priority, contents):
+    trig = '\t\t\t<trigger id="' + triggerId + '" priority="' + priority + '">\n'
+    trig += contents
+    trig += '\t\t\t</trigger>\n'
+    return trig
 
-	contents = makeTriggerContents(activities, conditions)
-	
-	triggerId = "BLC_Test_Block0_Trial" + str(i) + "_Trigger0"
-	code += makeTrigger(triggerId, "1", contents)
+def makeTriggerContents(activities, conditions):
+    return activities + conditions
 
-	"""
-	refStimulus = "BLC_Test_Block0_Trial" + str(i) + "_Trigger0_Sound_Stimulus"
-	activities = "\t\t\t\t<activities>\n"
-	activities += "\t\t\t\t\t" + makeActivtyRefOnly(refStimulus)
-	activities += "\t\t\t\t</activities>\n"
+def makeCondition(contents):
+    return '<condition>' + contents + '</condition>\n'
 
-	conditionContents = "collideWith:BLC_Test_Block0_Trial" + str(i) +"_Stimulus_Trigger0"
-	conditions = "\t\t\t\t" + makeCondition(conditionContents)
+def makeSignTimerActivities():
+    activities = ""
+    for i in range(18):
+        sign = "BLC_Test_Block0_Trial" + str(i) + "_ContainerElement0"
+        params = makeParameter("visible", "true")
+        params += makeParameter("id", sign)
+        contents = makeAction("0.0", "manipulateObject", "1", params)
+        
+        sign = "BLC_Test_Block0_Trial" + str(i) + "_ContainerElement1"
+        params = makeParameter("visible", "true")
+        params += makeParameter("id", sign)
+        contents += makeAction("0.0", "manipulateObject", "1", params)
+        
+        sign = "BLC_Test_Block0_Trial" + str(i) + "_ContainerElement2"
+        params = makeParameter("visible", "true")
+        params += makeParameter("id", sign)
+        contents += makeAction("0.0", "manipulateObject", "1", params)
+        
+        activityId = "BLC_Test_Block0_Trial" + str(i) + "_Trigger0_makeVisible_BLC_Test_Block0_Trial" + str(i) + "_ContainerElements"
+        activities += makeActivity(activityId, "", contents)
 
-	contents = makeTriggerContents(activities, conditions)
-	
-	triggerId = "BLC_Test_Block0_Trial" + str(i) + "_Stimulus_Trigger0"
-	code += makeTrigger(triggerId, "1", contents)
-	"""
+        params = makeParameter("minSteeringAngle", "0")
+        params += makeParameter("targetLane", targetLaneList[i])
+        params += makeParameter("startLane", startLaneList[i])
+        params += makeParameter("taskCompletionAfterDistance", "117")
+        params += makeParameter("taskCompletionAfterTime", "7000")
+        params += makeParameter("timerID", "timer1")
+        params += makeParameter("allowBreak", "true")
+        params += makeParameter("successSound", "good")
+        params += makeParameter("congruenceClass", "LaneChange")
+        params += makeParameter("holdLaneFor", "2000")
+        params += makeParameter("failSound", "fail")
+        contents = makeAction("0.0", "setupLaneChangeReactionTimer", "1", params)
+        
+        activityId = "BLC_Test_Block0_Trial" + str(i) + "_Trigger0_setupBrakeLaneChangeReactionTimer"
+        activities += makeActivity(activityId, "", contents)
 
-code += '\t</triggers>\n'
-code += '</interaction>\n'
+        """
+        params = makeParameter("soundID", soundIDList[i]);
+        contents = makeAction("0.0", "playSound", "1", params);
+        activityId = "BLC_Test_Block0_Trial" + str(i) + "_Trigger0_Sound_Stimulus"
+        activities += makeActivity(activityId, "", contents)
+        """
+    return activities
 
+def makeTriggerActivities():
+    triggers = ""
+    for i in range(18):
+        refElements = "BLC_Test_Block0_Trial" + str(i) + "_Trigger0_makeVisible_BLC_Test_Block0_Trial" + str(i) + "_ContainerElements"
+        refReactionTimer = "BLC_Test_Block0_Trial" + str(i) + "_Trigger0_setupBrakeLaneChangeReactionTimer"
+        activities = "\t\t\t\t<activities>\n"
+        activities += "\t\t\t\t\t" + makeActivtyRefOnly(refElements)
+        activities += "\t\t\t\t\t" + makeActivtyRefOnly(refReactionTimer)
+        activities += "\t\t\t\t</activities>\n"
 
-fd = open(filename, "w")
-fd.write(code)
-fd.close()
+        conditionContents = "collideWith:BLC_Test_Block0_Trial" + str(i) +"_Trigger0"
+        conditions = "\t\t\t\t" + makeCondition(conditionContents)
+
+        contents = makeTriggerContents(activities, conditions)
+        
+        triggerId = "BLC_Test_Block0_Trial" + str(i) + "_Trigger0"
+        triggers += makeTrigger(triggerId, "1", contents)
+
+        """
+        refStimulus = "BLC_Test_Block0_Trial" + str(i) + "_Trigger0_Sound_Stimulus"
+        activities = "\t\t\t\t<activities>\n"
+        activities += "\t\t\t\t\t" + makeActivtyRefOnly(refStimulus)
+        activities += "\t\t\t\t</activities>\n"
+
+        conditionContents = "collideWith:BLC_Test_Block0_Trial" + str(i) +"_Stimulus_Trigger0"
+        conditions = "\t\t\t\t" + makeCondition(conditionContents)
+
+        contents = makeTriggerContents(activities, conditions)
+        
+        triggerId = "BLC_Test_Block0_Trial" + str(i) + "_Stimulus_Trigger0"
+        triggers += makeTrigger(triggerId, "1", contents)
+        """
+    return triggers
+
+name = raw_input("Version number? ")
+
+if name != "":
+    interactionFilename = "interaction-" + name + ".xml"
+else:
+    interactionFilename = "interaction.xml"
+
+interactionfd = open(interactionFilename, "w")
+
+interactionfd.write(prologue)
+interactionfd.write(activitiesOpenTag)
+interactionfd.write(startActivities)
+interactionfd.write(makeSignTimerActivities())
+interactionfd.write(activitiesCloseTag)
+interactionfd.write(triggersOpenTag)
+interactionfd.write(startTrig)
+interactionfd.write(endTrig)
+interactionfd.write(makeTriggerActivities())
+interactionfd.write(triggerCloseTag)
+interactionfd.write(epilogue)
+
+interactionfd.close()
